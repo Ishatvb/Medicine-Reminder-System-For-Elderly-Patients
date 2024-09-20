11a4555a389668e1e9fe75029dffe21e200ccdeb
Ishatvb <Ishatvbeohar22082003@gmail.com>
2024-09-21 00:01:49 +0530

    authentication done

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,7 +3,7 @@ import {Button, Text, View} from 'react-native'
 function HomeScreen(){
     return(
         <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-            <Text style={{color:"black",fontSize:22}}>Home Screen</Text>
+            <Text style={{color:"black",fontSize:22}}>1. Home Screen</Text>
         </View>
     );
 }
