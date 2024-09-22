5a7c5142f0af72f8f19f1b05d0712704edd4adc1
Ishatvb <Ishatvbeohar22082003@gmail.com>
2024-09-22 05:34:07 +0530

    Stuling and Profile screen integration

--- App.js
+++ App.js
@@ -67,7 +67,7 @@ export default function App(){
 
             return <IconComponent name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: 'red',
+        tabBarActiveTintColor: '#420475',
         tabBarInactiveTintColor: 'black',
         tabBarLabelStyle: {
             fontSize: 14,
@@ -119,48 +119,3 @@ export default function App(){
 
     );
 }
-
-
-
-// import React, { useEffect } from 'react';
-// import { View, Text, StyleSheet, Alert } from 'react-native';
-// import Tts from 'react-native-tts';
-
-// function Rnvoice() {
-//   useEffect(() => {
-//     Tts.getInitStatus().then(() => {
-//       Tts.setDefaultLanguage('en-US'); 
-//       Tts.setDefaultRate(0.5); 
-//     }).catch((error) => {
-      
-//       Alert.alert('Error', 'Text-to-Speech initialization failed');
-//       console.error('TTS Error: ', error);
-//     });
-//   }, []);
-
-//   const handleVoice = ttsText => {
-//     Tts.speak(ttsText);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       {/* <Text style={styles.text} onPress={() => handleVoice('namaste, abb aapki dhavaai lenney ka sahmaayy hoooh ghayaa haaee. kripaya paraacetamol khanane ke baad len. dhanyavaad.')}> */}
-//       <Text style={styles.text} onPress={() => handleVoice('Hello, it is time to take your medicine. Please take paracetamol after meals. Thank you.')}>
-//         Reminder
-//       </Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   text: {
-//     fontSize: 22,
-//   },
-// });
-
-// export default Rnvoice;
